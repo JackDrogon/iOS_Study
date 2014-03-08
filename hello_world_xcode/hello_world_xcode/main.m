bdab5bf52c04a7fe844c6457e2f71d101f96c3c3
JackDrogon <jack.xsuperman@gmail.com>
2014-03-08 15:21:29 +0800

    Initial Commit

---
+++ hello_world_xcode/hello_world_xcode/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  hello_world_xcode
+//
+//  Created by Drogon on 3/8/14.
+//  Copyright (c) 2014 Drogon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
